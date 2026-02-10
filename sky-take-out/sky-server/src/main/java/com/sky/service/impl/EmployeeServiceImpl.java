da0f2968975a81e46458982bb18082e412641505
Txydw <919517056@qq.com>
2026-02-10 16:06:42 +0800

    增添记录修改人 ID 功能

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -84,9 +85,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateTime(LocalDateTime.now());
 
         //设置创建用户和修改用户
-        // TODO 后续修改真正的 ID
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L); 
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId()); 
 
         employeeMapper.insert(employee);
 
